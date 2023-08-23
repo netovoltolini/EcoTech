836435dc1b325e8824061df3d803fcc0848cf7ed
Vinícius Ribeiro <viniciusri2012@gmail.com>
2023-08-23 07:42:46 -0300

    Adicionado Visual do SubMenu da Navbar

--- app/src/main/java/com/example/eco_tech/Navbar.kt
+++ app/src/main/java/com/example/eco_tech/Navbar.kt
@@ -14,22 +14,27 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.eco_tech.ui.theme.Eco_TechTheme
 
 
 @Composable
 fun Navbar() {
+
     Column {
         Row(
             modifier = Modifier
-                .background(Color(0xFF3dd7b7))
+                .background(Color(0xFF3DD7B7))
                 .fillMaxWidth(),
             horizontalArrangement = Arrangement.SpaceBetween,
             verticalAlignment = Alignment.CenterVertically
@@ -43,39 +48,63 @@ fun Navbar() {
             )
             BarsButton()
         }
+        SubMenu()
     }
 }
 
 @Composable
 fun BarsButton() {
+
     Column(
         modifier = Modifier
             .width(66.dp)
             .height(40.dp)
             .padding(end = 20.dp)
             .border(width = 1.dp, color = Color(0xBFFFFFFF)),
-        verticalArrangement = Arrangement.Center
+        verticalArrangement = Arrangement.SpaceEvenly
     ) {
-        Box(
-            modifier = Modifier
-                .align(Alignment.CenterHorizontally)
-                .size(20.dp, 2.dp)
-                .background(Color(0xBFFFFFFF))
-        )
-        Spacer(modifier = Modifier.height(6.dp))
-        Box(
-            modifier = Modifier
-                .align(Alignment.CenterHorizontally)
-                .size(20.dp, 2.dp)
-                .background(Color(0xBFFFFFFF))
-        )
-        Spacer(modifier = Modifier.height(6.dp))
-        Box(
-            modifier = Modifier
-                .align(Alignment.CenterHorizontally)
-                .size(20.dp, 2.dp)
-                .background(Color(0xBFFFFFFF))
-        )
+        repeat(3) {
+            Box(
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .size(20.dp, 2.dp)
+                    .background(Color(0xBFFFFFFF))
+            )
+        }
+    }
+}
+
+@Composable
+fun SubMenu() {
+
+    val menuItems = listOf(
+        "Início",
+        "Receitas",
+        "Aproveitamento Integral",
+        "Agricultura Sustentável",
+        "Consulta",
+        "Busca"
+    )
+
+    Column(
+        modifier = Modifier
+            .background(Color(0xFF3dd7b7))
+            .fillMaxWidth(),
+        horizontalAlignment = Alignment.End
+    ) {
+        Column(
+            modifier = Modifier.padding(end = 20.dp, top = 10.dp),
+            horizontalAlignment = Alignment.End
+        ) {
+            menuItems.forEach { item ->
+                Text(
+                    modifier = Modifier.padding(bottom = 25.dp),
+                    text = item,
+                    color = Color.White,
+                    fontSize = 18.sp
+                )
+            }
+        }
     }
 }
 
